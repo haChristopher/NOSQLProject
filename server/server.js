d55107abd769f120b542128427871384165c6404
christopher.hansen <christopher.hansen@porschedigitallab.com>
2017-12-03 17:28:28 +0100

    added server chat demo

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    // emit sends messages from the server to the connected sockes
+    // the broadcast.emit function is a good idea for the channels
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
