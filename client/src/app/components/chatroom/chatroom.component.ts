6390ee2313a003c1b3dba270a3a158db630ea0ce
christopher.hansen <christopher.hansen@porschedigitallab.com>
2017-12-30 20:36:08 +0100

    Simple message sending over websocket is possible now

--- client/src/app/components/chatroom/chatroom.component.ts
+++ client/src/app/components/chatroom/chatroom.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CreateChannelComponent } from '../modals/create-channel.component';
+import * as io from "socket.io-client";
 
 @Component({
   moduleId: module.id,
@@ -8,8 +9,29 @@ import { CreateChannelComponent } from '../modals/create-channel.component';
   styleUrls: ['chatroom.component.css']
 })
 export class ChatroomComponent {
+  message = '';
+  conversation = [];
+  socket = null;
 
-  private createChannel(){
+  ngOnInit() {
+    this.socket = io('http://localhost:3000');
+    this.socket.on('chat message', function(data) {
+        this.conversation.push(data);
+    }.bind(this));
+}
 
-  }
+send() {
+    this.socket.emit('chat message', {
+        'userName': 'chris',
+        'text': this.message
+    });
+    this.message = '';
+}
+
+keypressHandler(event) {
+    if (event.keyCode === 13){
+        this.send();
+    }
+}
+
 }
