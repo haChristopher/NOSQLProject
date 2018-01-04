ebcc90afe7cec78c03d310cb5490d166995eeb1e
christopher.hansen <christopher.hansen@porschedigitallab.com>
2018-01-04 20:09:31 +0100

    added sql connection

--- Chatapp/server/mySqlDB.js
+++ Chatapp/server/mySqlDB.js
@@ -10,10 +10,17 @@ function db() {
 	var con = mysql.createPool({
 		host : 'localhost',
 	  	user : 'root',
-	  	password : '',
-	  	database : 'chat'
+	  	password : 'root',
+			database : 'chat'
 	});
 	this.connection=con;
+
+	// Database setup
+	con.query("CREATE TABLE IF NOT EXISTS user (id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,username VARCHAR(50),password TEXT,reg_date TIMESTAMP)", function (err, result) {
+		if (err) throw err;
+		console.log("Table created");
+	});
+
 }
 method.getcon = function() {
 	return this;
