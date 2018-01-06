4392f8b0caeeaf392234e8219cfb3cdaa3780bc5
christopher.hansen <christopher.hansen@porschedigitallab.com>
2018-01-06 15:00:19 +0100

    cors now working

--- Chatapp/app.js
+++ Chatapp/app.js
@@ -3,25 +3,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const http = require('http');
-
+const redis = require('redis');
 const app = express();
-
-//CORS middleware
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'example.com');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-  next();
-};
-
-app.use(allowCrossDomain);
-
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 
 // API file for interacting with MongoDB
 const history = require('./server/routes/history');
 const user = require('./server/routes/users');
-const messages = require('./server/routes/messages');
+
+//Create connection to redis for session handling
+app.use(session({
+    store: new RedisStore({ host: 'localhost', port: 6379, client: redis.createClient()}),
+    secret: 'Chillig',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+var cors = require('cors');
+// use it before all route definitions
+app.use(cors({origin: '*'}));
 
 //start Socketserver
 var socketServer = require('./server/connections/socketIoServer');
@@ -39,10 +46,12 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // API location
 app.use('/api', history);
 app.use('/api/sql', user);
-app.use('/api/messages', messages);
 
 // Send all other requests to the Angular app
+// Also create Session
 app.get('*', (req, res) => {
+  //req.session.key=req.body.email;
+  console.log("session created");
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
